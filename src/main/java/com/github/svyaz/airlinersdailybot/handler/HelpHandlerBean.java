7361f95808a77b38cb5ce6f0143387a77f91b3e8
Max Kuzhelev <maxim-kuzhelev@yandex.ru>
2024-10-19 00:38:38 +0700

    HelpHandlerBean implemented

--- src/main/java/com/github/svyaz/airlinersdailybot/handler/HelpHandlerBean.java
+++ src/main/java/com/github/svyaz/airlinersdailybot/handler/HelpHandlerBean.java
@@ -1,14 +1,24 @@
 package com.github.svyaz.airlinersdailybot.handler;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 @Service(UpdateHandler.HELP_COMMAND_HANDLER)
 public class HelpHandlerBean extends AbstractUpdateHandler {
 
+    @SneakyThrows
     @Override
     public void handle(Update update, AbsSender sender) {
+        var message = SendMessage.builder()
+                .parseMode("HTML")
+                .chatId(update.getMessage().getChatId())
+                .text(getMessage("help.text", null))
+                .replyMarkup(getButtons())
+                .build();
 
+        sender.execute(message);
     }
 }
