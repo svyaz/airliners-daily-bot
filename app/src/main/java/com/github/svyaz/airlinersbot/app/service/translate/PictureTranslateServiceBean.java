c1b8a0f879139e1fb570afbf5dab5cb99cbff2bd
Max Kuzhelev <maxim-kuzhelev@yandex.ru>
2025-06-27 01:08:10 +0700

    Добавлено кэширование для переведенных картинок

--- app/src/main/java/com/github/svyaz/airlinersbot/app/service/translate/PictureTranslateServiceBean.java
+++ app/src/main/java/com/github/svyaz/airlinersbot/app/service/translate/PictureTranslateServiceBean.java
@@ -2,23 +2,27 @@ package com.github.svyaz.airlinersbot.app.service.translate;
 
 import com.github.svyaz.airlinersbot.app.domain.Picture;
 import com.github.svyaz.airlinersbot.app.domain.Translatable;
+import com.github.svyaz.airlinersbot.conf.CacheConfig;
 import com.github.svyaz.airlinersbot.conf.TranslationConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PictureTranslateServiceBean implements PictureTranslateService {
 
     private final TranslationConfig translationConfig;
 
+    @Cacheable(value = CacheConfig.PICTURE_LANG_CACHE, key = "#picture.id + '_' + #langCode")
     @Override
-    //todo : тут кэширование
     public Picture translate(Picture picture, String langCode) {
         return Optional.of(langCode)
                 .filter(Predicate.not(code -> translationConfig.getDefaultLangCode().equals(code)))
---
+++ app/src/main/java/com/github/svyaz/airlinersbot/conf/CacheConfig.java
@@ -0,0 +1,11 @@
+package com.github.svyaz.airlinersbot.conf;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    public static final String PICTURE_LANG_CACHE = "PICTURE_LANG_CACHE";
+}
